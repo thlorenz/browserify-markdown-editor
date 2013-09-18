2d244e08b7ab8bad1f6946213c392cd57591210c
Thorsten Lorenz <thlorenz@gmx.de>
2013-09-17 21:23:49 -0400

    serving bundle as well

--- index.js
+++ index.js
@@ -4,9 +4,22 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 
-var server = http.createServer(function (req, res) {
+function serveIndex (res) {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   fs.createReadStream(path.join(__dirname, 'index.html')).pipe(res); 
+}
+
+function serveBundle (res) {
+  res.writeHead(200, { 'Content-Type': 'application/javascript' });
+  fs.createReadStream(path.join(__dirname, 'bundle.js')).pipe(res); 
+}
+
+var server = http.createServer(function (req, res) {
+  console.log('%s %s', req.method, req.url);
+  if (req.url === '/') return serveIndex(res);
+  if (req.url === '/bundle.js') return serveBundle(res);
+  res.writeHead(404);
+  res.end();
 });
 
 server.on('listening', function (address) {
