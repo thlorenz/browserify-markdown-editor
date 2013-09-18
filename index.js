f25936bc77f3ef9e8296b068247241ebb6ac9d1e
Thorsten Lorenz <thlorenz@gmx.de>
2013-09-17 21:55:51 -0400

    serving rendered markdown inside of index.html

--- index.js
+++ index.js
@@ -3,10 +3,30 @@
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
+var renderMd = require('./render-md');
+
+function serveError (res, err) {
+  res.writeHead(500, { 'Content-Type': 'text/plain' });
+  res.end(err.toString());
+}
 
 function serveIndex (res) {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  fs.createReadStream(path.join(__dirname, 'index.html')).pipe(res); 
+
+  fs.readFile(path.join(__dirname, 'index.html'), 'utf8', function (err, html) {
+    if (err) return serveError(err);
+    renderNserve(html);    
+  });
+
+  function renderNserve(html) {
+    fs.readFile(path.join(__dirname, 'README.md'), 'utf8', function (err, md) {
+      if (err) return serveError(err);
+
+      var rendered = renderMd(md);
+
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(html.replace(/{MARKDOWN}/, rendered));
+    });
+  }
 }
 
 function serveBundle (res) {
